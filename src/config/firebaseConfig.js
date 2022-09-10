82c435a01741ebefb8216c96964e34421731e843
Your Name <you@example.com>
2022-09-10 16:44:46 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB-jxSfqs-_m7PjsSYpuOxku50-h6Ya0w4",
+  authDomain: "crypto-hunter-1a7d2.firebaseapp.com",
+  projectId: "crypto-hunter-1a7d2",
+  storageBucket: "crypto-hunter-1a7d2.appspot.com",
+  messagingSenderId: "869428875235",
+  appId: "1:869428875235:web:d65c164717ca31044f5ad6",
+};
+
+export default firebaseConfig;
